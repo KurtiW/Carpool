56af4fea2f55a5c16351eaf181b08a9594985346
KurtiW <46797399+KurtiW@users.noreply.github.com>
2019-08-18 21:46:38 +0200

    Update mySeats.php

--- mySeats.php
+++ mySeats.php
@@ -1,7 +1,8 @@
 <?php
 
 include('connect.php');
-if(isset($_GET['user'])
+
+if(isset($_GET['user']))
 {
     $sql = "SELECT * FROM `PLACES` WHERE `USER_ID` = '".$_GET['user']."';";
 
@@ -17,13 +18,33 @@ if(isset($_GET['user'])
         {
             while($row = $result->fetch_assoc()) 
             {
-                echo $row["ID"] . "|" . $row["RIDES_ID"] . ";<br>";
+                
+                $sql = "SELECT * FROM `RIDES` WHERE `ID` = '".$row["RIDES_ID"]."'";
+    
+                $r = $conn->query($sql);
+                if(!$r)
+                { //$execute_query instead of execute_query
+                 die("error ".mysqli_error($conn));
+                 echo "query error";
+                }
+                else
+                {
+                    if ($r->num_rows > 0) 
+                    {
+                        while($row_ = $r->fetch_assoc()) 
+                        {
+                            echo $row_["ID"] . "|" . $row_["COUNT"]. "|" . $row_["OWNER"]. "|" . $row_["START"]. "|" . $row_["END"]. "|" . $row_["DEPARTURE"] . ";<br>";
+                        }
+                    }
+                }
+                
+                
             }
         }
     }
    
 }
-   else
+else
 {
     echo "query error";
 
